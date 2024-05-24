b8fa591f9ef9cbe7a737c687788c6210dc18b9d4
Jaya <jayababushrestha@gmail.com>
2024-05-24 13:29:36 +1000

    looping error is not coming at this stage but the table functionality  is not complete

--- ui/nodes/tableNode/TableNodeEdit.tsx
+++ ui/nodes/tableNode/TableNodeEdit.tsx
@@ -26,8 +26,6 @@ import {
   ImportButton
 } from '@/ui/nodes/tableNode/TableNodeToolbar';
 import { SketchPicker } from 'react-color';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
@@ -69,25 +67,6 @@ interface TableNodeEditProps extends NodeProps {
   ) => void;
   position: { x: number; y: number };
 }
-
-const DraggableRow = ({ index, moveRow, ...props }) => {
-  const ref = useRef(null);
-  const [, drop] = useDrop({
-    accept: 'row',
-    hover(item: { index: number }) {
-      if (item.index !== index) {
-        moveRow(item.index, index);
-        item.index = index;
-      }
-    }
-  });
-  const [, drag] = useDrag({
-    type: 'row',
-    item: { index }
-  });
-  drag(drop(ref));
-  return <div ref={ref} {...props} />;
-};
 
 const TableNodeEdit: React.FC<TableNodeEditProps> = ({
   data,
@@ -215,13 +194,6 @@ const TableNodeEdit: React.FC<TableNodeEditProps> = ({
     }
   }, []);
 
-  const moveRow = (dragIndex, hoverIndex) => {
-    const newRows = [...content.rows];
-    const [draggedRow] = newRows.splice(dragIndex, 1);
-    newRows.splice(hoverIndex, 0, draggedRow);
-    setContent({ ...content, rows: newRows });
-  };
-
   // Define custom CSS properties
   const customStyles: CSSProperties = {
     width: nodeWidth,
@@ -231,133 +203,105 @@ const TableNodeEdit: React.FC<TableNodeEditProps> = ({
   };
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div
-        className={styles.tableNode}
-        style={customStyles}
-        onClick={handleContainerClick}
-        onBlur={handleContainerBlur}
-      >
-        <NodeResizer
-          isVisible={isContainerSelected}
-          minWidth={200}
-          minHeight={200}
-          onResize={handleResize}
-          onResizeEnd={() => {
-            onNodeResizeStop(
-              data.id,
-              { width: nodeWidth, height: nodeHeight },
-              position
-            );
-          }}
+    <div
+      className={styles.tableNode}
+      style={customStyles}
+      onClick={handleContainerClick}
+      onBlur={handleContainerBlur}
+    >
+      <NodeResizer
+        isVisible={isContainerSelected}
+        minWidth={200}
+        minHeight={200}
+        onResize={handleResize}
+        onResizeEnd={() => {
+          onNodeResizeStop(
+            data.id,
+            { width: nodeWidth, height: nodeHeight },
+            position
+          );
+        }}
+      />
+      <div className={styles.header}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => onChangeTitle(e.target.value)}
+          className={`${styles.titleInput} nodrag`}
+          style={{ color: textColor }}
         />
-        <div className={styles.header}>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => onChangeTitle(e.target.value)}
-            className={`${styles.titleInput} nodrag`}
-            style={{ color: textColor }}
-          />
-          <CloseButton
-            onClick={() => handleClose(data.id, () => {}, title, content)}
-          />
-        </div>
-        <div className={styles.toolbar}>
-          <AddColumnButton onClick={() => addColumn(content, setContent)} />
-          <AddRowButton onClick={() => addRow(content, setContent)} />
-          <ExportButton onClick={() => exportTableData(content)} />
-          <ImportButton onChange={(e) => importTableData(e, setContent)} />
-        </div>
-        <div className={`${styles.tableContent} nowheel nodrag`}>
-          <div
-            className="ag-theme-alpine"
-            style={{ height: '100%', width: '100%' }}
-          >
-            <AgGridReact
-              columnDefs={content.columns.map((col) => ({
-                ...col,
-                editable: true,
-                resizable: true,
-                cellStyle: { borderRight: '1px solid #ccc' }
-              }))}
-              rowData={content.rows}
-              onGridReady={(params) => params.api.sizeColumnsToFit()}
-              domLayout="autoHeight"
-              defaultColDef={{
-                editable: true,
-                resizable: true
-              }}
-              components={{
-                rowRenderer: (params) => (
-                  <DraggableRow
-                    index={params.node.rowIndex}
-                    moveRow={moveRow}
-                    {...params}
-                  />
-                )
-              }}
-            />
-          </div>
-        </div>
-        <div className={styles.footer}>
-          <SaveButton
-            onClick={() =>
-              handleSave(data.id, () => {}, {
-                title,
-                content,
-                tags,
-                attachedFiles
-              })
-            }
-          />
-          <DeleteButton onClick={() => handleDelete(data.id, () => {})} />
-          <ChangeColorButton onClick={() => toggleColorPicker()} />
-          <AddTagButton onClick={() => handleAddTag(data.id, tags, onAddTag)} />
-          <AttachFileButton
-            onChange={(e) => handleAttachFile(data.id, onAttachFiles)(e)}
-          />
-          <DuplicateButton onClick={() => handleDuplicate(data.id)} />
-          {isColorPickerVisible && (
-            <div
-              className={`${styles.colorPicker} nodrag`}
-              ref={colorPickerRef}
-            >
-              <SketchPicker
-                color={backgroundColor}
-                onChangeComplete={handleChangeComplete}
-              />
-            </div>
-          )}
-        </div>
-        <div className={styles.tagContainer}>
-          {tags.map((tag, index) => (
-            <span
-              key={index}
-              className={styles.tag}
-              style={{ color: textColor }}
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
-        {attachedFiles.length > 0 && (
-          <div className={styles.attachedFile} style={{ color: textColor }}>
-            Attached files: {attachedFiles.map((file) => file.name).join(', ')}
-          </div>
-        )}
-        <Handle
-          type="target"
-          position={Position.Top}
-          className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleTop}`}
-        />
-        <Handle
-          type="source"
-          position={Position.Bottom}
-          className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleBottom}`}
+        <CloseButton
+          onClick={() => handleClose(data.id, () => {}, title, content)}
         />
       </div>
-    </DndProvider>
+      <div className={styles.toolbar}>
+        <AddColumnButton onClick={() => addColumn(content, setContent)} />
+        <AddRowButton onClick={() => addRow(content, setContent)} />
+        <ExportButton onClick={() => exportTableData(content)} />
+        <ImportButton onChange={(e) => importTableData(e, setContent)} />
+      </div>
+      <div className={`${styles.tableContent} nowheel nodrag`}>
+        <div
+          className="ag-theme-alpine"
+          style={{ height: '100%', width: '100%' }}
+        >
+          <AgGridReact
+            columnDefs={content.columns}
+            rowData={content.rows}
+            domLayout="autoHeight"
+          />
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <SaveButton
+          onClick={() =>
+            handleSave(data.id, () => {}, {
+              title,
+              content,
+              tags,
+              attachedFiles
+            })
+          }
+        />
+        <DeleteButton onClick={() => handleDelete(data.id, () => {})} />
+        <ChangeColorButton onClick={() => toggleColorPicker()} />
+        <AddTagButton onClick={() => handleAddTag(data.id, tags, onAddTag)} />
+        <AttachFileButton
+          onChange={(e) => handleAttachFile(data.id, onAttachFiles)(e)}
+        />
+        <DuplicateButton onClick={() => handleDuplicate(data.id)} />
+        {isColorPickerVisible && (
+          <div className={`${styles.colorPicker} nodrag`} ref={colorPickerRef}>
+            <SketchPicker
+              color={backgroundColor}
+              onChangeComplete={handleChangeComplete}
+            />
+          </div>
+        )}
+      </div>
+      <div className={styles.tagContainer}>
+        {tags.map((tag, index) => (
+          <span key={index} className={styles.tag} style={{ color: textColor }}>
+            {tag}
+          </span>
+        ))}
+      </div>
+      {attachedFiles.length > 0 && (
+        <div className={styles.attachedFile} style={{ color: textColor }}>
+          Attached files: {attachedFiles.map((file) => file.name).join(', ')}
+        </div>
+      )}
+      <Handle
+        type="target"
+        position={Position.Top}
+        className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleTop}`}
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleBottom}`}
+      />
+    </div>
   );
 };
 
