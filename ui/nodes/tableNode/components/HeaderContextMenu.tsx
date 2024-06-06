1c5b0a9b1ec4c67548d98fd263c3e83ce450b370
Jaya <jayababushrestha@gmail.com>
2024-06-07 07:11:19 +1000

    making changes...

--- ui/nodes/tableNode/components/HeaderContextMenu.tsx
+++ ui/nodes/tableNode/components/HeaderContextMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Item, Separator } from 'react-contexify';
+import { Menu, Item, Separator, Submenu } from 'react-contexify';
 import 'react-contexify/ReactContexify.css';
 import styles from '@/ui/nodes/tableNode/styles/HeaderContextMenu.module.css';
 import Portal from '@/ui/nodes/tableNode/Portal';
@@ -16,21 +16,81 @@ const HeaderContextMenu = ({
   onAlignCenter,
   onAlignRight
 }) => {
+  const itemStyle = {
+    padding: '8px 16px',
+    cursor: 'pointer'
+  };
+
   return (
     <Portal>
       <Menu id={id} className={styles.contextMenu}>
-        <Item onClick={onSortAsc}>Sort Ascending</Item>
-        <Item onClick={onSortDesc}>Sort Descending</Item>
-        <Item onClick={onFilter}>Filter</Item>
+        <Item onClick={onSortAsc} style={itemStyle} className={styles.item}>
+          Sort Ascending
+        </Item>
+        <Item onClick={onSortDesc} style={itemStyle} className={styles.item}>
+          Sort Descending
+        </Item>
+        <Item onClick={onFilter} style={itemStyle} className={styles.item}>
+          Filter
+        </Item>
         <Separator />
-        <Item onClick={onRename}>Rename Column</Item>
-        <Item onClick={onChangeType}>Change Datatype</Item>
+        <Item onClick={onRename} style={itemStyle} className={styles.item}>
+          Rename Column
+        </Item>
+        <Submenu
+          label="Change Datatype"
+          style={{ minWidth: '100px' }} // Inline style for submenu width
+        >
+          <Item
+            onClick={() => onChangeType('text')}
+            style={itemStyle}
+            className={styles.item}
+          >
+            Text
+          </Item>
+          <Item
+            onClick={() => onChangeType('number')}
+            style={itemStyle}
+            className={styles.item}
+          >
+            Number
+          </Item>
+          <Item
+            onClick={() => onChangeType('email')}
+            style={itemStyle}
+            className={styles.item}
+          >
+            Email
+          </Item>
+          <Item
+            onClick={() => onChangeType('date')}
+            style={itemStyle}
+            className={styles.item}
+          >
+            Date
+          </Item>
+          <Item
+            onClick={() => onChangeType('currency')}
+            style={itemStyle}
+            className={styles.item}
+          >
+            Currency
+          </Item>
+        </Submenu>
         <Separator />
-        <Item onClick={onAlignLeft}>Align Left</Item>
-        <Item onClick={onAlignCenter}>Align Center</Item>
-        <Item onClick={onAlignRight}>Align Right</Item>
+        <Item onClick={onAlignLeft} style={itemStyle} className={styles.item}>
+          Align Left
+        </Item>
+        <Item onClick={onAlignCenter} style={itemStyle} className={styles.item}>
+          Align Center
+        </Item>
+        <Item onClick={onAlignRight} style={itemStyle} className={styles.item}>
+          Align Right
+        </Item>
         <Separator />
-        <Item onClick={onDelete}>Delete Column</Item>
+        <Item onClick={onDelete} style={itemStyle} className={styles.item}>
+          Delete Column
+        </Item>
       </Menu>
     </Portal>
   );
