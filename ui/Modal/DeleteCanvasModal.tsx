7dca97a471a18dc5633b86cc8f13ae872cbfa569
Jaya <jayababushrestha@gmail.com>
2024-07-13 18:56:49 +1000

    making changes...

--- ui/Modal/DeleteCanvasModal.tsx
+++ ui/Modal/DeleteCanvasModal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Modal } from 'react-responsive-modal';
+import { Dialog, Transition } from '@headlessui/react';
 import Button from '@/ui/Button/Button';
-import styles from '@/ui/Modal/Modal.module.css';
+import styles from '@/ui/Modal/DeleteCanvasModal.module.css';
+import { IoMdClose } from 'react-icons/io';
 
 const DeleteCanvasModal = ({
   isOpen,
@@ -10,49 +11,88 @@ const DeleteCanvasModal = ({
   hasNodes,
   hasSharedNodes
 }) => (
-  <Modal open={isOpen} onClose={onClose} center>
-    <h2>Confirm Deletion</h2>
-    {hasNodes ? (
-      <>
-        <p>
-          This canvas contains nodes. Do you want to delete the canvas only or
-          delete the canvas and its nodes (except shared ones)?
-        </p>
-        {hasSharedNodes && (
-          <p>
-            Some nodes in this canvas are shared with other canvases. These
-            shared nodes will not be deleted.
-          </p>
-        )}
-        <div className={styles.actions}>
-          <Button variant="submit" onClick={() => onConfirm('canvasOnly')}>
-            Delete Canvas Only
-          </Button>
-          <Button variant="danger" onClick={() => onConfirm('withNodes')}>
-            Delete Canvas and Nodes (except shared ones)
-          </Button>
-          <Button variant="cancel" onClick={onClose}>
-            Cancel
-          </Button>
-        </div>
-      </>
-    ) : (
-      <>
-        <p>
-          Are you sure you want to delete this canvas? This action cannot be
-          undone.
-        </p>
-        <div className={styles.actions}>
-          <Button variant="submit" onClick={() => onConfirm('canvasOnly')}>
-            Yes
-          </Button>
-          <Button variant="cancel" onClick={onClose}>
-            Cancel
-          </Button>
-        </div>
-      </>
-    )}
-  </Modal>
+  <Transition.Root show={isOpen} as={React.Fragment}>
+    <Dialog as="div" className={styles.modalOverlay} onClose={onClose}>
+      <div className={styles.modalWrapper}>
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className={styles.modalBackground} />
+        </Transition.Child>
+
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <div className={styles.modalContent}>
+            <button className={styles.closeButton} onClick={onClose}>
+              <IoMdClose />
+            </button>
+            <Dialog.Title as="h3" className={styles.modalHeader}>
+              Confirm Deletion
+            </Dialog.Title>
+            <div className="nodrag nowheel">
+              {hasNodes ? (
+                <>
+                  <p className={styles.modalText}>
+                    This canvas contains nodes. Do you want to delete the canvas
+                    only or delete the canvas and its nodes (except shared
+                    ones)?
+                  </p>
+                  {hasSharedNodes && (
+                    <p className={styles.modalText}>
+                      Some nodes in this canvas are shared with other canvases.
+                      These shared nodes will not be deleted.
+                    </p>
+                  )}
+                  <div className={styles.actions}>
+                    <Button
+                      variant="submit"
+                      onClick={() => onConfirm('canvasOnly')}
+                    >
+                      Delete Canvas Only
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => onConfirm('withNodes')}
+                    >
+                      Delete Canvas and Nodes
+                    </Button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p className={styles.modalText}>
+                    Are you sure you want to delete this canvas? This action
+                    cannot be undone.
+                  </p>
+                  <div className={styles.actions}>
+                    <Button
+                      variant="submit"
+                      onClick={() => onConfirm('canvasOnly')}
+                    >
+                      Yes, Delete Canvas
+                    </Button>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </Transition.Child>
+      </div>
+    </Dialog>
+  </Transition.Root>
 );
 
 export default DeleteCanvasModal;
