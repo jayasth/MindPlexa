f4249422f72653da8f605756c22333380c77f98d
Jaya <jayababushrestha@gmail.com>
2024-07-24 09:17:29 +1000

    making changes...

---
+++ ui/nodes/drawNode/DrawNodeEditOld.tsx
@@ -0,0 +1,425 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  CSSProperties,
+  useCallback,
+  useMemo
+} from 'react';
+import { NodeProps, Handle, Position, NodeResizer } from 'reactflow';
+import styles from './DrawNodeEdit.module.css';
+import edgeStyles from '@/ui/edges/CustomEdgeStyles.module.css';
+import {
+  FaPencilAlt,
+  FaPaintBrush,
+  FaMarker,
+  FaEraser,
+  FaSprayCan
+} from 'react-icons/fa';
+import { IoMdWater } from 'react-icons/io';
+import {
+  useBrush,
+  useMarker,
+  useAirbrush,
+  Artboard,
+  ArtboardRef,
+  useShadingBrush,
+  useEraser,
+  useWatercolor,
+  ToolHandlers
+} from '@/ui/nodes/drawNode/DrawNodeTools';
+import { useHistory } from '@/ui/nodes/drawNode/drawNodeHistory';
+import type { IconType } from 'react-icons/lib';
+import DrawNodeToolbar from '@/ui/nodes/drawNode/components/DrawNodeToolbar';
+import {
+  DeleteButton,
+  ChangeColorButton,
+  AddTagButton,
+  AttachFileButton,
+  CloseButton,
+  DuplicateButton,
+  TagModal,
+  FileModal,
+  ColorPickerModal
+} from '@/ui/nodes/common/CommonNodeComponents';
+import NodeDeleteConfirmationModal from '@/ui/nodes/common/NodeDeleteConfirmationModal';
+import TagFileContainer from '@/ui/nodes/common/TagFileContainer';
+import {
+  handleTitleChange,
+  handleClose,
+  handleDelete as handleDeleteNode,
+  colorCombinations,
+  handleAddTag,
+  handleDuplicate
+} from '@/ui/nodes/common/CommonNodeFunctions';
+import {
+  Attachment,
+  removeAttachment,
+  getAttachments,
+  removeAllPreviews
+} from '@/utils/canvas/attachmentService';
+import { useBackgroundColorChange } from '@/ui/nodes/common/useBackgroundColorChange';
+import { debounce } from 'lodash';
+import useNodeStore from '@/app/store/nodes/useNodeStore';
+import useCanvasStore from '@/app/store/canvas/useCanvasStore';
+
+interface DrawNodeEditProps extends NodeProps {
+  data: any;
+  width: number;
+  height: number;
+  selected: boolean;
+  onNodeResizeStop: (
+    nodeId: string,
+    newSize: { width: number; height: number },
+    newPosition: { x: number; y: number }
+  ) => void;
+  position: { x: number; y: number };
+}
+
+const DrawNodeEdit: React.FC<DrawNodeEditProps> = ({
+  data,
+  width,
+  height,
+  selected,
+  onNodeResizeStop,
+  position
+}) => {
+  console.log('DrawNodeEdit: Node details:', {
+    id: data.id,
+    title: data.title,
+    drawingData: data.drawingData,
+    backgroundColor: data.backgroundColor,
+    textColor: data.textColor,
+    width,
+    height,
+    position
+  });
+
+  const { canvasId } = useCanvasStore();
+  const [isSelected, setIsSelected] = useState(selected);
+  const [title, setTitle] = useState(data.title || 'Untitled Drawing');
+  const [drawingData, setDrawingData] = useState(data.drawingData || '');
+  const [backgroundColor, setBackgroundColor] = useState(
+    data.backgroundColor || '#F4F4F4'
+  );
+  const [textColor, setTextColor] = useState(data.textColor || '#575757');
+  const [tags, setTags] = useState<string[]>(data.tags || []);
+  const [attachedFiles, setAttachedFiles] = useState<Attachment[]>([]);
+  const [isContainerSelected, setIsContainerSelected] = useState(false);
+  const [nodeWidth, setNodeWidth] = useState(width);
+  const [nodeHeight, setNodeHeight] = useState(height);
+  const [isColorPickerVisible, setIsColorPickerVisible] = useState(false);
+  const [isTagModalOpen, setIsTagModalOpen] = useState(false);
+  const [isFileModalOpen, setIsFileModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [color, setColor] = useState('#531B93');
+  const [strokeWidth, setStrokeWidth] = useState(5);
+  const [currentTool, setCurrentTool] = useState(0);
+
+  const artboardRef = useRef<ArtboardRef | null>(null);
+
+  const handleBackgroundColorChange = useBackgroundColorChange(
+    data.id,
+    setBackgroundColor,
+    setTextColor
+  );
+
+  const onChangeColor = useCallback(
+    (color: { hex: string }) => {
+      handleBackgroundColorChange(color);
+    },
+    [handleBackgroundColorChange]
+  );
+
+  const debouncedUpdateNodeData = useMemo(
+    () =>
+      debounce(async (commonData, specificData) => {
+        try {
+          const updateNode = useNodeStore.getState().updateNode;
+          await updateNode(
+            data.id,
+            { ...commonData, data: specificData },
+            'draw'
+          );
+        } catch (error) {
+          console.error('Error updating node:', error);
+        }
+      }, 500),
+    [data.id]
+  );
+
+  useEffect(() => {
+    return () => {
+      debouncedUpdateNodeData.cancel();
+      removeAllPreviews();
+    };
+  }, [debouncedUpdateNodeData]);
+
+  useEffect(() => {
+    const commonData = {
+      title,
+      backgroundColor,
+      textColor,
+      editWidth: nodeWidth,
+      editHeight: nodeHeight
+    };
+
+    const specificData = { drawingData, tags, attachedFiles };
+
+    debouncedUpdateNodeData(commonData, specificData);
+  }, [
+    title,
+    drawingData,
+    backgroundColor,
+    textColor,
+    nodeWidth,
+    nodeHeight,
+    tags,
+    attachedFiles,
+    debouncedUpdateNodeData
+  ]);
+
+  const onChangeTitle = useCallback(
+    (newTitle: string) => {
+      handleTitleChange(data.id, newTitle, setTitle, canvasId);
+    },
+    [data.id, canvasId]
+  );
+
+  const onAddTag = useCallback(
+    (newTags: string[]) => {
+      const uniqueTags = Array.from(new Set([...tags, ...newTags]));
+      setTags(uniqueTags);
+      handleAddTag(data.id, uniqueTags, () => {}, canvasId);
+    },
+    [data.id, tags, canvasId]
+  );
+
+  const onRemoveTag = useCallback(
+    (tagToRemove: string) => {
+      const updatedTags = tags.filter((tag) => tag !== tagToRemove);
+      setTags(updatedTags);
+      handleAddTag(data.id, updatedTags, () => {}, canvasId);
+    },
+    [data.id, tags, canvasId]
+  );
+
+  const onAttachFiles = useCallback(async (files: Attachment[]) => {
+    setAttachedFiles(files);
+  }, []);
+
+  const onRemoveFile = useCallback(
+    async (fileId: string) => {
+      await removeAttachment(fileId);
+      const updatedAttachments = await getAttachments(data.id);
+      setAttachedFiles(updatedAttachments);
+    },
+    [data.id]
+  );
+
+  useEffect(() => {
+    const fetchAttachments = async () => {
+      const attachments = await getAttachments(data.id);
+      setAttachedFiles(attachments);
+    };
+    fetchAttachments();
+  }, [data.id]);
+
+  useEffect(() => {
+    setNodeWidth(width);
+    setNodeHeight(height);
+  }, [width, height]);
+
+  const handleResize = useCallback(
+    (event, { width, height }) => {
+      setNodeWidth(width);
+      setNodeHeight(height);
+      onNodeResizeStop(data.id, { width, height }, position);
+    },
+    [data.id, onNodeResizeStop, position]
+  );
+
+  const handleContainerClick = useCallback(() => {
+    setIsContainerSelected(true);
+  }, []);
+
+  const handleContainerBlur = useCallback(() => {
+    setIsContainerSelected(false);
+  }, []);
+
+  const toggleColorPicker = useCallback(() => {
+    setIsColorPickerVisible((prev) => !prev);
+  }, []);
+
+  const customStyles: CSSProperties = useMemo(
+    () => ({
+      width: nodeWidth,
+      height: nodeHeight,
+      backgroundColor,
+      color: textColor
+    }),
+    [nodeWidth, nodeHeight, backgroundColor, textColor]
+  );
+
+  const handleDelete = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteConfirm = () => {
+    setIsDeleteModalOpen(false);
+    handleDeleteNode(data.id, canvasId);
+  };
+
+  const handleDeleteCancel = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const brush = useBrush({ color, strokeWidth });
+  const marker = useMarker({ color, strokeWidth });
+  const watercolor = useWatercolor({ color, strokeWidth });
+  const airbrush = useAirbrush({ color, strokeWidth });
+  const eraser = useEraser({ strokeWidth });
+  const shading = useShadingBrush({
+    color,
+    spreadFactor: (1 / 45) * strokeWidth,
+    distanceThreshold: 100
+  });
+
+  const tools: Array<[ToolHandlers, IconType, number]> = [
+    [shading, FaPencilAlt, 5],
+    [watercolor, IoMdWater, 20],
+    [brush, FaPaintBrush, 15],
+    [marker, FaMarker, 20],
+    [airbrush, FaSprayCan, 10],
+    [eraser, FaEraser, 30]
+  ];
+
+  const { undo, redo, history, canUndo, canRedo } = useHistory();
+
+  const memoizedTagFileContainer = useMemo(
+    () => (
+      <TagFileContainer
+        tags={tags}
+        attachedFiles={attachedFiles}
+        onRemoveTag={onRemoveTag}
+        onRemoveFile={onRemoveFile}
+        textColor={textColor}
+      />
+    ),
+    [tags, attachedFiles, onRemoveTag, onRemoveFile, textColor]
+  );
+
+  return (
+    <div
+      className={styles.drawNode}
+      style={customStyles}
+      onClick={handleContainerClick}
+      onBlur={handleContainerBlur}
+    >
+      <NodeResizer
+        isVisible={isContainerSelected}
+        minWidth={200}
+        minHeight={200}
+        onResize={handleResize}
+      />
+      <div className={styles.header}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => onChangeTitle(e.target.value)}
+          className={`${styles.titleInput} nodrag`}
+          style={{ color: textColor }}
+        />
+        <CloseButton
+          onClick={() =>
+            handleClose(data.id, () => {}, title, drawingData, canvasId)
+          }
+        />
+      </div>
+      <div className={`${styles.drawContent} nowheel nodrag`}>
+        <DrawNodeToolbar
+          tools={tools}
+          currentTool={currentTool}
+          setCurrentTool={setCurrentTool}
+          color={color}
+          setColor={setColor}
+          strokeWidth={strokeWidth}
+          setStrokeWidth={setStrokeWidth}
+          undo={undo}
+          redo={redo}
+          canUndo={canUndo}
+          canRedo={canRedo}
+          download={() => artboardRef.current?.download()}
+          clear={() => artboardRef.current?.clear()}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+          onZoomIn={() => {}}
+          onZoomOut={() => {}}
+          layers={[]}
+          activeLayerId="0" 
+          setLayers={() => {}}
+          setActiveLayerId={() => {}}
+        />
+        <div id="artboard" className={styles.artboard}>
+          <Artboard
+            tool={tools[currentTool][0]}
+            ref={artboardRef}
+            history={history}
+            style={{ border: '1px gray solid' }}
+            content={drawingData}
+            width={nodeWidth / 2}
+            height={nodeHeight / 2}
+          />
+        </div>
+      </div>
+      {(tags.length > 0 || attachedFiles.length > 0) &&
+        memoizedTagFileContainer}
+      <div className={styles.footer}>
+        <DeleteButton onClick={() => setIsDeleteModalOpen(true)} />
+        <ChangeColorButton onClick={toggleColorPicker} />
+        <AddTagButton onClick={() => setIsTagModalOpen(true)} />
+        <AttachFileButton onClick={() => setIsFileModalOpen(true)} />
+        <DuplicateButton onClick={() => handleDuplicate(data.id, canvasId)} />
+        <ColorPickerModal
+          isOpen={isColorPickerVisible}
+          onClose={() => setIsColorPickerVisible(false)}
+          currentColor={backgroundColor}
+          onChangeColor={onChangeColor}
+          colorCombinations={colorCombinations}
+        />
+      </div>
+      <Handle
+        type="target"
+        position={Position.Top}
+        className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleTop}`}
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        className={`${edgeStyles.reactFlowHandle} ${edgeStyles.reactFlowHandleBottom}`}
+      />
+      <TagModal
+        isOpen={isTagModalOpen}
+        onClose={() => setIsTagModalOpen(false)}
+        onAddTag={onAddTag}
+        onRemoveTag={onRemoveTag}
+        existingTags={tags}
+      />
+      <FileModal
+        isOpen={isFileModalOpen}
+        onClose={() => setIsFileModalOpen(false)}
+        onAttachFiles={onAttachFiles}
+        onRemoveFile={onRemoveFile}
+        existingFiles={attachedFiles}
+        nodeId={data.id}
+      />
+      <NodeDeleteConfirmationModal
+        isOpen={isDeleteModalOpen}
+        onClose={handleDeleteCancel}
+        onConfirm={handleDeleteConfirm}
+      />
+    </div>
+  );
+};
+
+export default React.memo(DrawNodeEdit);
---
+++ ui/nodes/drawNode/components/DrawNodeArtboardOld.tsx
@@ -0,0 +1,298 @@
+import React, {
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useState,
+  useEffect
+} from 'react';
+
+import { History } from '@/ui/nodes/drawNode/drawNodeHistory';
+
+import {
+  getMousePoint,
+  getTouchPoint,
+  mouseButtonIsDown,
+  Point
+} from '@/ui/nodes/drawNode/utils/pointUtils';
+
+export interface ArtboardProps
+  extends React.CanvasHTMLAttributes<HTMLCanvasElement> {
+  tool: ToolHandlers;
+  history?: History;
+  onStartStroke?: (point: Point) => void;
+  onContinueStroke?: (point: Point) => void;
+  onEndStroke?: () => void;
+  content?: string;
+  onContentChange?: (newContent: string) => void;
+  width: number;
+  height: number;
+  onResize?: () => void;
+}
+
+export interface ArtboardRef {
+  download: (filename?: string, type?: string) => void;
+  getImageAsDataUri: (type?: string) => string | undefined;
+  clear: () => void;
+  context?: CanvasRenderingContext2D | null;
+  width: number;
+  height: number;
+}
+
+export interface ToolHandlers {
+  name: string;
+  startStroke?: (point: Point, context: CanvasRenderingContext2D) => void;
+  continueStroke?: (point: Point, context: CanvasRenderingContext2D) => void;
+  endStroke?: (context: CanvasRenderingContext2D) => void;
+  cursor?: string;
+}
+
+export const Artboard = forwardRef(function Artboard(
+  {
+    tool,
+    style,
+    history,
+    onStartStroke,
+    onContinueStroke,
+    onEndStroke,
+    content,
+    onContentChange,
+    width,
+    height,
+    onResize,
+    ...props
+  }: ArtboardProps,
+  ref: ForwardedRef<ArtboardRef>
+) {
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>();
+  const [canvas, setCanvas] = useState<HTMLCanvasElement>();
+  const [drawing, setDrawing] = useState(false);
+
+  const startStroke = useCallback(
+    (point: Point) => {
+      if (!context) {
+        return;
+      }
+      context.save();
+      setDrawing(true);
+      tool.startStroke?.(point, context);
+      onStartStroke?.(point);
+    },
+    [tool, context, onStartStroke]
+  );
+
+  const continueStroke = useCallback(
+    (newPoint: Point) => {
+      if (!context) {
+        return;
+      }
+      tool.continueStroke?.(newPoint, context);
+      onContinueStroke?.(newPoint);
+    },
+    [tool, context, onContinueStroke]
+  );
+
+  const endStroke = useCallback(() => {
+    setDrawing(false);
+    if (context) {
+      tool.endStroke?.(context);
+      onEndStroke?.();
+      context.restore();
+      if (canvas && history) {
+        history.pushState(canvas);
+      }
+      if (onContentChange) {
+        const newContent = canvas?.toDataURL() || '';
+        onContentChange(newContent);
+        window.dispatchEvent(
+          new CustomEvent('content-updated', {
+            detail: { content: newContent }
+          })
+        );
+      }
+    }
+  }, [tool, context, canvas, history, onEndStroke, onContentChange]);
+
+  const mouseMove = useCallback(
+    (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (!drawing) {
+        return;
+      }
+      continueStroke(getMousePoint(event));
+    },
+    [continueStroke, drawing]
+  );
+
+  const touchMove = useCallback(
+    (event: React.TouchEvent) => {
+      if (!drawing) {
+        return;
+      }
+      continueStroke(getTouchPoint(event));
+    },
+    [continueStroke, drawing]
+  );
+
+  const mouseDown = useCallback(
+    (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (drawing) {
+        return;
+      }
+      event.preventDefault();
+      startStroke(getMousePoint(event));
+    },
+    [drawing, startStroke]
+  );
+
+  const touchStart = useCallback(
+    (event: React.TouchEvent) => {
+      if (drawing) {
+        return;
+      }
+      startStroke(getTouchPoint(event));
+    },
+    [drawing, startStroke]
+  );
+
+  const clear = useCallback(() => {
+    if (!context || !canvas) {
+      return;
+    }
+    context.save();
+    context.fillStyle = '#ffffff';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.restore();
+    if (canvas && history) {
+      history.pushState(canvas);
+    }
+    if (onContentChange) {
+      const newContent = canvas?.toDataURL() || '';
+      onContentChange(newContent);
+      window.dispatchEvent(
+        new CustomEvent('content-updated', { detail: { content: newContent } })
+      );
+    }
+  }, [context, canvas, history, onContentChange]);
+
+  const gotRef = useCallback(
+    (canvasRef: HTMLCanvasElement) => {
+      if (!canvasRef) {
+        return;
+      }
+      const aspectRatio = 16 / 9;
+      const canvasSize = Math.min(width, height);
+      canvasRef.width = canvasSize * aspectRatio;
+      canvasRef.height = canvasSize;
+      const ctx = canvasRef.getContext('2d');
+      setCanvas(canvasRef);
+      setContext(ctx);
+      if (!ctx) {
+        return;
+      }
+      ctx.fillStyle = '#ffffff';
+      ctx.fillRect(0, 0, canvasRef.width, canvasRef.height);
+      ctx.fillStyle = 'transparent';
+      if (content) {
+        const image = new Image();
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0, canvasRef.width, canvasRef.height);
+        };
+        image.src = content;
+      }
+      if (history) {
+        history.setContext(ctx);
+        history.pushState(canvasRef);
+      }
+    },
+    [width, height, content, history]
+  );
+
+  const mouseEnter = useCallback(
+    (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (mouseButtonIsDown(event.buttons)) {
+        mouseDown(event);
+      } else if (drawing) {
+        endStroke();
+      }
+    },
+    [drawing, mouseDown, endStroke]
+  );
+
+  const mouseLeave = useCallback(
+    (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (!drawing) {
+        return;
+      }
+      continueStroke(getMousePoint(event));
+      endStroke();
+    },
+    [continueStroke, drawing, endStroke]
+  );
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      download: (filename = 'image.png', type?: string) => {
+        if (!canvas) {
+          return;
+        }
+        const a = document.createElement('a');
+        a.href = canvas.toDataURL(type);
+        a.download = filename;
+        a.click();
+      },
+      clear,
+      getImageAsDataUri: (type?: string) => canvas?.toDataURL(type),
+      context,
+      width: canvas?.width || 0,
+      height: canvas?.height || 0
+    }),
+    [canvas, context, clear]
+  );
+
+  useEffect(() => {
+    if (onResize) {
+      onResize();
+    }
+    if (canvas && context) {
+      const image = new Image();
+      image.onload = () => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(
+          image,
+          0,
+          0,
+          image.width,
+          image.height,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        ); // Draw the image with scaling
+      };
+      image.src = content || '';
+    }
+  }, [width, height, onResize, canvas, context, content]);
+
+  useEffect(() => {
+    if (onContentChange) {
+      onContentChange(canvas?.toDataURL() || '');
+    }
+  }, [canvas, onContentChange]);
+
+  return (
+    <canvas
+      style={{ cursor: tool?.cursor, touchAction: 'none', ...style }}
+      onTouchStart={touchStart}
+      onMouseDown={mouseDown}
+      onMouseEnter={mouseEnter}
+      onMouseMove={drawing ? mouseMove : undefined}
+      onTouchMove={drawing ? touchMove : undefined}
+      onMouseUp={endStroke}
+      onMouseOut={mouseLeave}
+      onTouchEnd={endStroke}
+      ref={gotRef}
+      {...props}
+    />
+  );
+});
