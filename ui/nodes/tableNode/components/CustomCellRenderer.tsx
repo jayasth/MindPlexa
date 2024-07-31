e6fa564f6b60844785c32fa430a26c7ae2ba9d16
Jaya <jayababushrestha@gmail.com>
2024-07-31 20:43:49 +1000

    making changes..

--- ui/nodes/tableNode/components/CustomCellRenderer.tsx
+++ ui/nodes/tableNode/components/CustomCellRenderer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Tooltip } from '@/ui/Tooltip/Tooltip';
-import styles from '@/ui/nodes/tableNode/styles/CustomCellRenderer.module.css';
 
 const CustomCellRenderer = (props) => {
   const handleCellClick = () => {
@@ -10,26 +8,10 @@ const CustomCellRenderer = (props) => {
     });
   };
 
-  const isInvalid =
-    props.node.data.invalid && props.column.colId === props.invalidColumn;
-  const tooltipContent = isInvalid
-    ? `Invalid value for column type "${props.column.colDef.type}": ${props.value}`
-    : null;
-
-  const cellContent = (
-    <div
-      onClick={handleCellClick}
-      onDoubleClick={handleCellClick}
-      className={isInvalid ? styles.invalidCell : ''}
-    >
+  return (
+    <div onClick={handleCellClick} onDoubleClick={handleCellClick}>
       {props.value !== undefined && props.value !== null ? props.value : ''}
     </div>
-  );
-
-  return isInvalid ? (
-    <Tooltip content={tooltipContent}>{cellContent}</Tooltip>
-  ) : (
-    cellContent
   );
 };
 
